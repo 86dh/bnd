6bb393289fd63b4a634e7908201af481d185303c
Jared Anderson <jhanders@us.ibm.com>
2026-07-07 12:46:29 -0500

    Update to not set the index until it has completed opening
    Signed-off-by: Jared Anderson <jhanders@us.ibm.com>

--- biz.aQute.repository/src/aQute/bnd/repository/maven/provider/MavenBndRepository.java
+++ biz.aQute.repository/src/aQute/bnd/repository/maven/provider/MavenBndRepository.java
@@ -746,8 +746,10 @@ public class MavenBndRepository extends BaseRepository implements RepositoryPlug
 			}
 			Set<String> multi = Strings.splitAsStream(configuration.multi())
 				.collect(Sets.toSet());
-			this.index = new IndexFile(domain, reporter, indexFile, source, storage, client.promiseFactory(), multi);
-			this.index.open();
+			// Set the index only after it completes opening
+			IndexFile newIndex = new IndexFile(domain, reporter, indexFile, source, storage, client.promiseFactory(), multi);
+			newIndex.open();
+			this.index = newIndex;
 
 			try (Formatter f = new Formatter()) {
 				validateUris(release, f);
